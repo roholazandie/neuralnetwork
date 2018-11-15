4d2fae0baf0046c34e1cf66927f5ea0c27417766
Mohsen.SharifiRenani <mohsen.sharifi91@gmail.com>
2018-11-14 22:13:01 -0700

    I updated the code_last version

--- src/cnn.py
+++ src/cnn.py
@@ -1,7 +1,8 @@
 import tensorflow
-from keras.optimizers import RMSprop
+from keras.engine.saving import model_from_json
+from keras.optimizers import RMSprop, Adam
 from keras.preprocessing.image import ImageDataGenerator
-
+from visualization.plotlyvisualize import plot_confusion_matrix
 from src.neural_network_datasets import MNISTDataset
 import matplotlib.pyplot as plt
 from keras import losses, optimizers, metrics
@@ -9,6 +10,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, MaxPooling2D
 from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import TensorBoard, ReduceLROnPlateau
+from sklearn.metrics import confusion_matrix
 from keras import backend as K
 import numpy as np
 import os
@@ -25,7 +27,7 @@ n_epochs = 10
 batch_size = 50
 
 # Center
-centered_data_dir = "../data/centered_data/"
+centered_data_dir = "R:/Research Common/EBL/Researcher/Mohsen/Class/Fall 2018/Neural Network/project02/project022/data/centered_data/"
 centered_mnist_dataset = MNISTDataset(dataset_dir= centered_data_dir)
 
 centered_train_data, centered_train_labels = centered_mnist_dataset.load_train_dataset()
@@ -41,7 +43,7 @@ one_hotted_centered_test_labels = centered_mnist_dataset.one_hot_encode_dataset(
 one_hotted_centered_validation_labels = centered_mnist_dataset.one_hot_encode_dataset(centered_validation_labels)
 
 # UnCenter
-uncentered_data_dir = "../data/uncentered_data/"
+uncentered_data_dir = "R:/Research Common/EBL/Researcher/Mohsen/Class/Fall 2018/Neural Network/project02/project022/data/uncentered_data/"
 uncentered_mnist_dataset = MNISTDataset(dataset_dir= uncentered_data_dir)
 
 uncentered_train_data, uncentered_train_labels = uncentered_mnist_dataset.load_train_dataset()
@@ -83,7 +85,7 @@ one_hotted_centered_uncentered_test_labels = one_hotted_centered_uncentered_test
 one_hotted_centered_uncentered_validation_labels = one_hotted_centered_uncentered_validation_labels[shuffle_index_validation]
 
 # Position and Size Invariance dataset
-psi_data_dir = "../data/position_and_size_invariance/"
+psi_data_dir = "R:/Research Common/EBL/Researcher/Mohsen/Class/Fall 2018/Neural Network/project02/project022/data/position_and_size_invariance/"
 psi_mnist_dataset = MNISTDataset(dataset_dir= psi_data_dir)
 
 psi_train_data, psi_train_labels = psi_mnist_dataset.load_train_dataset()
@@ -180,7 +182,7 @@ elif architechure == "model2":
     model.add(Dense(256, activation="relu"))
     model.add(Dropout(0.5))
     model.add(Dense(10, activation="softmax"))
-    optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    optimizer = Adam(lr=0.001, epsilon=1e-08, decay=0.0)
     model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
 
     learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
@@ -192,20 +194,20 @@ elif architechure == "model2":
     n_epochs = 30
     batch_size = 86
 
-    datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        # zca_whitening=False,  # apply ZCA whitening
-        # rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-        # zoom_range=0.1,  # Randomly zoom image
-        # width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        # height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        # horizontal_flip=False,  # randomly flip images
-        # vertical_flip=False
-        )
-    datagen.fit(psi_train_data)
+    # datagen = ImageDataGenerator(
+    #     featurewise_center=False,  # set input mean to 0 over the dataset
+    #     samplewise_center=False,  # set each sample mean to 0
+    #     featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    #     samplewise_std_normalization=False,  # divide each input by its std
+    #     # zca_whitening=False,  # apply ZCA whitening
+    #     # rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+    #     # zoom_range=0.1,  # Randomly zoom image
+    #     # width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    #     # height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    #     # horizontal_flip=False,  # randomly flip images
+    #     # vertical_flip=False
+    #     )
+    # datagen.fit(psi_train_data)
 
 
 # # Center Model
@@ -249,31 +251,92 @@ elif architechure == "model2":
 
 
 #Position invariance
-tensorboard = TensorBoard(log_dir="../logs/logs_cnn_psi")
-# model.fit(psi_train_data, one_hotted_psi_train_labels,
-#           batch_size=batch_size,
-#           epochs=n_epochs,
-#           verbose=1,
-#           validation_data=(psi_validation_data, one_hotted_psi_validation_labels),
-#           callbacks=[tensorboard])
+tensorboard = TensorBoard(log_dir="logs/logs_cnn_model2")
+model.fit(psi_train_data, one_hotted_psi_train_labels,
+          batch_size=batch_size,
+          epochs=n_epochs,
+          verbose=1,
+          validation_data=(psi_validation_data, one_hotted_psi_validation_labels),
+          callbacks=[tensorboard])
 
 
-history = model.fit_generator(datagen.flow(psi_train_data,
-                      one_hotted_psi_train_labels,
-                      batch_size = batch_size),
-                      epochs = n_epochs,
-                      validation_data = (psi_validation_data, one_hotted_psi_validation_labels),
-                      verbose = 2,
-                      steps_per_epoch=psi_train_data.shape[0] // batch_size,
-                      callbacks=[learning_rate_reduction, tensorboard])
-
-#score = model.evaluate(centered_uncentered_test_data, one_hotted_centered_uncentered_test_labels, verbose=0)
-#print('loss:', score[0], 'categorical_accuracy:', score[1])
+# history = model.fit(psi_train_data,
+#                       one_hotted_psi_train_labels,
+#                       batch_size = batch_size,
+#                       epochs = n_epochs,
+#                       validation_data = (psi_validation_data, one_hotted_psi_validation_labels),
+#                       verbose = 2,
+#                       #steps_per_epoch=psi_train_data.shape[0] // batch_size,
+#                       callbacks=[learning_rate_reduction, tensorboard])
 
 
 
-json_string = model.to_json()
-name = "rohola_mohsen"
-open(name + '_architecture.json','w').write(json_string)
+# json_string = model.to_json()
+# name = "rohola_mohsen"
+# open(name + '_architecture.json','w').write(json_string)
+#
+# model.save_weights(name + '_weights.h5')
 
-model.save_weights(name + '_weights.h5')
+
+##################################################################
+# json_file = open(r'C:\Users\Mohsen.SharifiRenani\Desktop\pycharmprojects\neuralnetwork3\models\rohola_mohsen_architecture.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# model = model_from_json(loaded_model_json)
+# model.load_weights(r"C:\Users\Mohsen.SharifiRenani\Desktop\pycharmprojects\neuralnetwork3\models\rohola_mohsen_weights.h5")
+# # Predict the values from the validation dataset
+# Y_pred = model.predict(psi_validation_data)
+# # Convert predictions classes to one hot vectors
+# Y_pred_classes = np.argmax(Y_pred, axis = 1)
+# # Convert validation observations to one hot vectors
+# Y_true = np.argmax(one_hotted_psi_validation_labels, axis = 1)
+# # compute the confusion matrix
+# confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+# # plot the confusion matrix
+# plot_confusion_matrix(confusion_mtx, classes = range(10))
+#
+# ##################################################################
+# # Display some error results
+#
+# # Errors are difference between predicted labels and true labels
+# errors = (Y_pred_classes - Y_true != 0)
+#
+# Y_pred_classes_errors = Y_pred_classes[errors]
+# Y_pred_errors = Y_pred[errors]
+# Y_true_errors = Y_true[errors]
+# X_val_errors = psi_validation_data[errors]
+#
+# def display_errors(errors_index,img_errors,pred_errors, obs_errors):
+#     """ This function shows 6 images with their predicted and real labels"""
+#     n = 0
+#     nrows = 2
+#     ncols = 3
+#     fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
+#     for row in range(nrows):
+#         for col in range(ncols):
+#             error = errors_index[n]
+#             ax[row,col].imshow((img_errors[error]).reshape((28,28)))
+#             ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
+#             n += 1
+#
+#
+#     fig.savefig('wrong_samples1.png')
+#     plt.show()
+#
+# # Probabilities of the wrong predicted numbers
+# Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
+#
+# # Predicted probabilities of the true values in the error set
+# true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+#
+# # Difference between the probability of the predicted label and the true label
+# delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+#
+# # Sorted list of the delta prob errors
+# sorted_dela_errors = np.argsort(delta_pred_true_errors)
+#
+# # Top 6 errors
+# most_important_errors = sorted_dela_errors[-6:]
+#
+# # Show the top 6 errors
+# display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
